7ffdc838dc973db226e0ee4b8f31d32efd495d7a
Maxence Larrieu <m@larri.eu>
2019-11-20 13:38:02 +0100

    Update python_sword.py

--- python_sword.py
+++ python_sword.py
@@ -5,24 +5,26 @@ out = open('04-zconsol.txt', 'w')
 
 url = 'https://api-preprod.archives-ouvertes.fr/sword/hal'
 service = 'https://api.archives-ouvertes.fr/sword/servicedocument'
-
+	
+#ou application/zip pour charger les fichiers
 headers = {
     'Packaging': 'http://purl.org/net/sword-types/AOfr',
-    'Content-Type': 'text/xml', #ou application/zip pour charger les fichiers
+    'Content-Type': 'text/xml',
 }
 
-#data = open('./TEI/source/art2.xml')
-data = open('ART.xml', encoding='UTF-8')
+
+#data = open('ART.xml', encoding='utf-8')
+xmlfile = open('./TEI/ART.xml', encoding='utf-8')
+print(xmlfile)
 print('TEI has been loaded')
 
-response = requests.post(url, headers=headers, data=data, auth=('urLogin', 'urpass'))
+response = requests.post(url, headers=headers, data=xmlfile, auth=('user', 'pass'))
 
+out.write(response.text)# copier la réponse de sword dans fichier txt
 
-out.write(response.text) #coller la réponse de SWORD dans fichier texte
-data.close()
-out.close()
 print("done")
-
+xmlfile.close()
+out.close()
 
 
 
@@ -30,7 +32,7 @@ print("done")
 documentation SWORD HAL : https://api.archives-ouvertes.fr/docs/sword
 fichier TEI pour TEST : https://api.archives-ouvertes.fr/documents/art.xml
 
-la méthode CURL présente dans la docs
+la méthode CURL présente dans la doc
 curl -X POST -d @meta.xml -v -u 
 login:password https://api.archives-ouvertes.fr/sword/hal/ 
 -H "Packaging:http://purl.org/net/sword-types/AOfr" 
